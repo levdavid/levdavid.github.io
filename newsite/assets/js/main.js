a5cacebfb14e5fb37705e9a393933c7df904ab21
lev david <levdavid1@gmail.com>
2016-02-15 14:56:11 -0500

    lev - added new site in a hidden path

---
+++ newsite/assets/js/main.js
@@ -0,0 +1,109 @@
+/*
+	Photon by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+
+jQuery(document).ready(function($){
+	var timelineBlocks = $('.cd-timeline-block'),
+		offset = 0.8;
+
+	//hide timeline blocks which are outside the viewport
+	hideBlocks(timelineBlocks, offset);
+
+	//on scolling, show/animate timeline blocks when enter the viewport
+	$(window).on('scroll', function(){
+		(!window.requestAnimationFrame) 
+			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
+			: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
+	});
+
+	function hideBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+		});
+	}
+
+	function showBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+		});
+	}
+});
+
+(function($) {
+
+	skel.breakpoints({
+		xlarge: '(max-width: 1680px)',
+		large: '(max-width: 1140px)',
+		medium: '(max-width: 980px)',
+		small: '(max-width: 736px)',
+		xsmall: '(max-width: 480px)',
+		xxsmall: '(max-width: 320px)'
+	});
+	
+	$('.skill').hover(
+		function(){$(this).addClass('colored');},
+		function(){$(this).removeClass('colored');}
+	);
+
+	$('#profecient').on("click", function(e){
+		$('#profecient').addClass('selected');
+		$('#familiar').removeClass('selected');
+		$('#profecientList').fadeIn();
+		$('#familiarList').hide();
+	});
+
+	$('#familiar').on("click", function(e){
+		$('#profecient').removeClass('selected');
+		$('#familiar').addClass('selected');
+		$('#profecientList').hide();
+		$('#familiarList').fadeIn();
+	});
+
+
+	// $(function(){
+	// 	$('.project').mouseenter(
+	// 		function(){
+	// 			$(this).find('.image').stop(true).animate({opacity:.3}, 250);
+	// 		}
+	// 	), 
+	// 	$('.project').mouseleave(
+	// 	function(){
+	// 		$(this).find('.image').stop(true).animate({opacity:1}, 250);
+	// 	});
+	// });
+
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 250);
+			});
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on mobile.
+			skel.on('+mobile -mobile', function() {
+				$.prioritize(
+					'.important\\28 mobile\\29',
+					skel.breakpoint('mobile').active
+				);
+			});
+
+		// Scrolly.
+			$('.scrolly').scrolly();
+
+	});
+
+})(jQuery);
